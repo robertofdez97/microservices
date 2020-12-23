9e2c41a746c1f6b8ee2fa33c401cdc274b21ab0d
rober <rober@DESKTOP-I84U4AN>
2020-12-23 18:47:52 +0100

    item project

---
+++ item/src/test/java/com/microservices/items/ItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
