c6656ce697e4da46721cc7ba7cdf482ae32b0d03
rober <rober@DESKTOP-PCSDAC4>
2020-12-29 23:45:16 +0100

    erekaServer

--- common/src/main/java/com/microservices/common/models/Item.java
+++ common/src/main/java/com/microservices/common/models/Item.java
@@ -1,15 +1,12 @@
 package com.microservices.common.models;
 
-import javax.persistence.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "products")
+@AllArgsConstructor
 public class Item {
 	private Product product;
 	private Integer quantity;
