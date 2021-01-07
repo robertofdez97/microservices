1591cc6c9b991b9ebee183d085d312d530f9b4bf
rober <robefe02@gmail.com>
2021-01-07 23:32:44 +0100

    details

--- users/src/main/java/com/microservices/users/repository/IUserRepository.java
+++ users/src/main/java/com/microservices/users/repository/IUserRepository.java
@@ -2,9 +2,11 @@ package com.microservices.users.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.microservices.users.models.User;
 
+@RepositoryRestResource(path = "user")
 public interface IUserRepository extends PagingAndSortingRepository<User, Long> {
 
 	public User findByUsername(String username);
